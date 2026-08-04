2a9b719111096391c1e8f21c90bde31d1bc0133f
André Fabiano <andrefsr1902@gmail.com>
2026-08-04 12:57:15 -0300

    .

---
+++ Estudos em SQL/mySQL/select_part2_aula12_ex.sql
@@ -0,0 +1,17 @@
+1 - Uma lista com o nome de todas as mulheres
+
+2 - Uma lista com os dados de todos aqueles que nasceram entre 1/jan/200 e 31/dez/2015
+
+3 - Uma lista com o nome de todos os homens que trabalham como programadores
+
+4 - Uma lista com os dados de todas as mulheres que nasceram no Brasil e que têm seu nome iniciando com a letra J
+
+5 - Uma lista com o nome e nacionalidade de todos os homens que têm Silva no nome, não nasceram no Brasil e pesam menos de 200 Kg
+
+6 - Qual é a maior altura entre homens que moram no Brasil?
+
+7 - Qual é a média de peso dos homens cadastrados?
+
+8 - Qual é o menor peso entre as mulheres que nasceram fora do Brasil e entre 01/Jan/1990 e 31/Dez/2000?
+
+9 - Quantas mulheres têm mais de 1.90m de altura?
